ac715e1ab1de6c1019c40a06f37e9dd4a5ec3860
zhaiduo <zhaiduo@gmail.com>
2016-01-11 22:14:57 +0800

    try to fix permission deny problem

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # haskellTest
+
+* How to add package?
+    edit bin path in .cabal/config
+    cabal sandbox init
+    cabal install moduleName
+
+* Problem: cabal: /usr/bin/ar: permission denied
+    Try https://github.com/haskell/cabal/issues/2653
+
+## resources
+* http://learnyouahaskell.com/starting-out#ready-set-go
+* https://wiki.haskell.org/Haskell_in_5_steps
--- compile.sh
+++ compile.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
-ghc --make test.hs -o dest/test
-rm -f test.hi
-rm -f test.o
+GHC="ghc";
+FILENAME=$1
+NAME=${FILENAME%.*}
+EXT=${FILENAME#*.}
+
+#https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html
+ARR=(${FILENAME//\// })
+if [ "${ARR[1]}" != "" ]; then
+    echo "Invalid path."
+    exit 1
+fi
+
+if [ "$EXT" != "hs" ]; then
+    echo "Invalid file name [.hs]."
+    exit 1
+fi
+
+if [ ! -f $FILENAME ]; then
+    echo "Not exist file."
+    exit 1
+fi
+
+if [ "$1" != "" ]; then
+    if [ "$1" = "maths.hs" ]; then
+        $GHC -O2 --make $1 -threaded -rtsopts -o "dest/$NAME"
+    else
+        $GHC --make $1 -o "dest/$NAME"
+    fi
+    rm -f "$NAME.hi"
+    rm -f "$NAME.o"
+else
+    $GHC --make test.hs -o dest/test
+    rm -f "test.hi"
+    rm -f "test.o"
+fi
+
